2bc0cf7b49ac50c5179a19aac8216ddd31921082
Martin Geiseler <hallo@tralafiti.de>
2012-12-22 16:57:12 +0100

    No more doublicate slashes in symlinkes paths

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 cp ~/.dotfiles/githook.post-commit ~/.dotfiles/.git/hooks/post-commit
 cp ~/.dotfiles/githook.post-merge ~/.dotfiles/.git/hooks/post-merge
 chmod +x ~/.dotfiles/.git/hooks/post-commit ~/.dotfiles/.git/hooks/post-merge
-find ~/.dotfiles/dots/ -type f -name ".*" -exec ln -sfi {} $HOME \;
+find ~/.dotfiles/dots -type f -name ".*" -exec ln -sfi {} $HOME \;
