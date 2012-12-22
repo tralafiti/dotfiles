4831e7fe54650cc338bbe3baa21cbedc17a35115
Martin Geiseler <hallo@tralafiti.de>
2012-12-22 18:17:08 +0100

    Githooks are now symlinks instead of copys allowing for updates without rerunning init.sh in the future.

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-cp ~/.dotfiles/githook.post-commit ~/.dotfiles/.git/hooks/post-commit
-cp ~/.dotfiles/githook.post-merge ~/.dotfiles/.git/hooks/post-merge
-chmod +x ~/.dotfiles/.git/hooks/post-commit ~/.dotfiles/.git/hooks/post-merge
-find ~/.dotfiles/dots -type f -name ".*" -exec ln -sfi {} $HOME \;
-find ~/.dotfiles/dots -type f -name "default.*" -exec sh -c 'cp -i $1 $HOME/.${1##*.}' funcname {} \;
+find ~/.dotfiles/githooks -type f -exec ln -sfi {} ~/.dotfiles/.git/hooks \;
+find ~/.dotfiles/dots -type f -name ".*" -exec ln -sfi {} ~ \;
+find ~/.dotfiles/dots -type f -name "default.*" -exec sh -c 'cp -i $1 ~/.${1##*.}' funcname {} \;
