c62b80ca87f74069454f4b189d36fab92588b906
Martin Geiseler <hallo@tralafiti.de>
2012-12-22 17:43:11 +0100

    Hooks now cp default.* files from dots directory into home.

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ cp ~/.dotfiles/githook.post-commit ~/.dotfiles/.git/hooks/post-commit
 cp ~/.dotfiles/githook.post-merge ~/.dotfiles/.git/hooks/post-merge
 chmod +x ~/.dotfiles/.git/hooks/post-commit ~/.dotfiles/.git/hooks/post-merge
 find ~/.dotfiles/dots -type f -name ".*" -exec ln -sfi {} $HOME \;
+find ~/.dotfiles/dots -type f -name "default.*" -exec sh -c 'cp -i $1 $HOME/.${1##*.}' funcname {} \;
