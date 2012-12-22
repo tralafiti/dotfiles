13ac1fdd5dc5cd0ea1c44b5f7118e5ba85e28b67
Martin Geiseler <hallo@tralafiti.de>
2012-12-22 18:28:01 +0100

    Zsh: New alias for updating of dotfiles

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -8,8 +8,10 @@ alias _='sudo'
 # List direcory contents
 alias ll='ls -lah'
 
+# Easy updating of dotfiles where ever you are
+alias update-dotfiles='(cd ~/.dotfiles; git pull)';
+
 # mkdir & cd to it
 function mcdir() {
     mkdir -p "$1" && cd "$1";
 }
-
