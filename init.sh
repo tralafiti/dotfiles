9c7526d2cb8804d1d4e2801650c135676ea6fe34
Martin Geiseler <m.geiseler@global-communication.de>
2019-06-28 13:22:19 +0200

    Add aliases and git-template

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 find ~/.dotfiles/githooks -type f -exec ln -sfi {} ~/.dotfiles/.git/hooks \;
-find ~/.dotfiles/dots -type f -name ".*" -exec ln -sfi {} ~ \;
-find ~/.dotfiles/dots -type f -name "default.*" -exec sh -c 'cp -i $1 ~/.${1##*.}' funcname {} \;
+find ~/.dotfiles/dots -name ".*" -prune -exec ln -sfi {} ~ \;
+find ~/.dotfiles/dots -name "default.*" -prune -exec sh -c 'cp -i $1 ~/.${1##*.}' funcname {} \;
--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -14,7 +14,17 @@ alias wget='curl -OLR'
 # Easy updating of dotfiles where ever you are
 alias update-dotfiles='(cd ~/.dotfiles; git pull)';
 
+# Set docker env vars for dev docker machine
+alias d-env='eval $(docker-machine env default)';
+
+alias gg='cd ~/Documents/Web/Global\ Group/projects/';
+
 # mkdir & cd to it
 function mcdir() {
     mkdir -p "$1" && cd "$1";
 }
+
+# Open in ...
+alias stree='open -a SourceTree'
+alias storm='open -a PhpStorm'
+
