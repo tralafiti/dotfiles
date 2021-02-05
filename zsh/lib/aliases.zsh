40764d0cf8473bc00afe4285a1288e2ba4193e73
Martin Geiseler <m.geiseler@global-communication.de>
2021-02-05 12:59:56 +0100

    ZSH-Completions, Docker Desktop (brrr)

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -12,7 +12,16 @@ alias ll='ls -lah'
 alias update-dotfiles='(cd ~/.dotfiles; git pull)';
 
 # Set docker env vars for dev docker machine
-alias d-env='eval $(docker-machine env default)';
+#alias d-env='eval $(docker-machine env default)';
+function d-up() {
+  open --background -a "Docker";
+  echo 'Docker For Desktop started. Waiting for hyperkit-VM to come up...'
+  while ! docker system info > /dev/null 2>&1; do sleep 1; done
+  echo 'Done'
+}
+function d-down() {
+  osascript -e 'quit app "Docker"';
+}
 # Set minishift oc env vars
 alias oc-env='eval $(minishift oc-env)';
 
@@ -20,7 +29,7 @@ alias gcs='cd ~/Development/GCS/projects/';
 
 # mkdir & cd to it
 function mcdir() {
-    mkdir -p "$1" && cd "$1";
+  mkdir -p "$1" && cd "$1";
 }
 
 # Open in ...
