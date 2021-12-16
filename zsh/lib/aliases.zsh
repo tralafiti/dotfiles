a3b46b13d849a793d6353ef635f9920dd97b70b2
Martin Geiseler <hallo@tralafiti.de>
2021-12-16 13:16:45 +0100

    Add alias for docker desktop vm "shell"

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -11,17 +11,21 @@ alias ll='ls -lah'
 # Easy updating of dotfiles where ever you are
 alias update-dotfiles='(cd ~/.dotfiles; git pull)';
 
-# Set docker env vars for dev docker machine
-#alias d-env='eval $(docker-machine env default)';
+# Start Docker Desktop silently
 function d-up() {
   open --hide --background -a "Docker";
   echo 'Docker For Desktop started. Waiting for hyperkit-VM to come up...'
   while ! docker system info > /dev/null 2>&1; do sleep 1; done
   echo 'Done'
 }
+# Stop Docker Desktop
 function d-down() {
   osascript -e 'quit app "Docker"';
 }
+
+# Get a shell within Doocker Desktop VM, see https://github.com/justincormack/nsenter1
+alias d-shell='docker run -it --rm --privileged --pid=host justincormack/nsenter1'
+
 # Set minishift oc env vars
 alias oc-env='eval $(minishift oc-env)';
 
