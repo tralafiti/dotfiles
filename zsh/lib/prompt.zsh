8f3fecfc44dbfc2811d4942666c4f70f4ba73e0c
Martin Geiseler <hallo@tralafiti.de>
2022-03-02 10:21:25 +0100

    Add modified kube-ps1 to prompt

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -2,6 +2,9 @@ setopt prompt_subst
 autoload -U promptinit
 promptinit
 
+autoload -U add-zsh-hook
+add-zsh-hook precmd _prompt
+
 ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[cyan]%}s"
@@ -13,5 +16,13 @@ ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[blue]%}+"
 ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[blue]%}-"
 ZSH_THEME_GIT_PROMPT_DIVERGED="%{$fg[blue]%}*"
 
-precmd() { print -rP '%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)'  }
+KUBE_PS1_SYMBOL_ENABLE=false
+KUBE_PS1_PREFIX=" to "
+KUBE_PS1_SUFFIX=""
+KUBE_PS1_CTX_COLOR="yellow"
+KUBE_PS1_NS_COLOR="red"
+
+_prompt() {
+  print -rP '%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)$(kube_ps1)'
+}
 PROMPT='%# '
