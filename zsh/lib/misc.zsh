0fc0d3c822834c53790aa86eaf75acf3e569f29a
Martin Geiseler <hallo@tralafiti.de>
2022-03-02 10:31:09 +0100

    Thank you for brewing

--- zsh/lib/misc.zsh
+++ zsh/lib/misc.zsh
@@ -16,5 +16,8 @@ export LC_CTYPE=$LANG
 # thank you docker inc for the free ads
 export DOCKER_SCAN_SUGGEST=false
 
+# stop it brew
+export HOMEBREW_NO_ENV_HINTS=true
+
 ## remove douplicate entries in path
 export PATH=$(awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}'<<<$PATH)
